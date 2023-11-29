4435cf0425e47a95526b23790a16cf30b4416509
darshan72 <darshankhairnar72@gmail.com>
2023-11-29 23:17:59 +0530

    added wave pattern

--- 2. Patterns/j22Pattern.java
+++ 2. Patterns/j22Pattern.java
@@ -1,45 +1,63 @@
 import java.util.Scanner;
 
 public class j22Pattern {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
 
         int length = in.nextInt();
         int height = in.nextInt();
 
-        for(int i = 1; i <= height; i++){
-            
+        for (int i = 1; i <= height; i++) {
+
+            for (int j = height - i; j >= 1; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= length; j++) {
+
+                System.out.print("*");
+
+                for (int k = 1; k <= 3 + (i - 2) * 4; k++) {
+                    System.out.print(" ");
+                }
+
+                if (i > 1 && i < height)
+                    System.out.print("*");
+
+                if (i == height && j == length) {
+                    System.out.print("*");
+                }
+
+                for (int k = 1; k <= 3 + ((height - i + 1) - 2) * 4; k++) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
         }
-
-
-
-
-
 
         in.close();
     }
 }
 
+// * *
+// * * * *
+// * * *
 
-    //        *      *
-    //      *   *  *   *
-    //    *      *      *
+// * *
+// * * *
 
+// *
+// * *
+// * *
 
-//   *   *
-// *   *   *
+// * *
+// * * *
+// * * *
+// * *
 
-//     *
-//   *   *
-// *       *
+// * * 0 =0 15 7 (n-i+1)
+// * * * 3 =1 11 5
+// * * * 7 =3 7 3
+// * * * 11=5 3 1
+// * * 15=7 0 0
 
-//       *           * 
-//     *   *       *
-//   *       *   *
-// *           *
-
-//         *               *        
-//       *   *           *          1*2+1
-//     *       *       *            3*2+1 
-//   *           *   *              5*2+1  
-// *               *                7*2+1 
+// 3+(i-2)*4
