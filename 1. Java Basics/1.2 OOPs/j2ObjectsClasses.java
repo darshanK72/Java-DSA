e77090cfd4c3419fe22dd521ef186b5062c6249c
Darshan Khairnar <darshankhairnar72@gmail.com>
2023-12-04 20:16:48 +0530

    ran all previous programs

--- 1. Java Basics/1.2 OOPs/j2ObjectsClasses.java
+++ 1. Java Basics/1.2 OOPs/j2ObjectsClasses.java
@@ -17,15 +17,61 @@ public class j2ObjectsClasses {
     }
 }
 
-class Student {
-    // Attributes, Instent variables
-    int roll_no;
+// class Student {
+//     // Attributes, Instent variables
+//     int roll_no;
+//     String name;
+//     float marks;
+
+//     // Class Methods
+//     void great_student() {
+//         System.out.println(
+//                 "Hello, " + name + ",\nYour Roll No is " + roll_no + "\nYou got " + marks + "% marks in this semester");
+//     }
+// }
+
+class Student
+{
     String name;
+    int roll_no;
     float marks;
 
-    // Class Methods
-    void great_student() {
-        System.out.println(
-                "Hello, " + name + ",\nYour Roll No is " + roll_no + "\nYou got " + marks + "% marks in this semester");
+    // Non-paramaterised Constructor
+    Student()
+    {
+        name = "Not Mentioned";
+        roll_no = 0;
+        marks = 0.0f;
+
+        System.out.println("Non-Parameterised Constructor Called!");
+    }
+
+    // Parameterised Constructor
+    Student(String name,int roll_no,float marks)
+    {
+        this.name = name;
+        this.roll_no = roll_no;
+        this.marks = marks;
+
+        System.out.println("Parameterised Constructor Called!");
+    }
+
+    // Copy Constructor
+    Student(Student s)
+    {
+        this.name = s.name;
+        this.roll_no = s.roll_no;
+        this.marks = s.marks;
+
+    }
+
+    void great_student()
+    {
+        System.out.println("Hello, " + name + ",\nYour Roll No is "+roll_no + "\nYou got "+ marks + "% marks in this semester");
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Hello, World");
     }
 }
