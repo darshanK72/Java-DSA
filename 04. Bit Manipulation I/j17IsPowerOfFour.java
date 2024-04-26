b9e2b1141af5130dfa158fd16379955d038a87a6
darshan72 <darshankhairnar72@gmail.com>
2024-04-26 23:54:09 +0530

    some bit manipulation questions

---
+++ 04. Bit Manipulation I/j17IsPowerOfFour.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class j17IsPowerOfFour{
+    public static void main(String args[]){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(isPowerOfFour(n));
+        in.close();
+    }
+
+    public static boolean isPowerOfFour(int n){
+        int mask = 0b01010101010101010101010101010101;
+        return n > 0 && (n & (n-1)) == 0 && (mask & n) > 0;
+    }
+}
