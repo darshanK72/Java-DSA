0ed6b05024c7267677df1b0286100640c8222ec2
Darshan Khairnar <darshankhairnar72@gmail.com>
2024-05-24 21:55:11 +0530

    contineuous left right angle triangle name change

--- 02. Patterns/j27continuousLeftRightTriangle.java
+++ 02. Patterns/j27continuousLeftRightTriangle.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class j27continuousLeftRightTriangle {
+public class j27ContinuousLeftRightTriangle {
    public static void main(String args[]){
         Scanner in = new Scanner(System.in);
 
