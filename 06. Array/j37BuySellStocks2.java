6598ae8332798fe2682af91844c6acc8b3d31fd7
Darshan Khairnar <darshankhairnar72@gmail.com>
2024-07-24 22:10:31 +0530

    revision complete

--- 06. Array/j37BuySellStocks2.java
+++ 06. Array/j37BuySellStocks2.java
@@ -15,7 +15,7 @@ public class j37BuySellStocks2{
     }
 
     // O(n)
-    public int maxProfit(int[] prices) {
+    public static int maxProfit(int[] prices) {
         int profit = 0;
         for(int i = 1; i < prices.length; i++){
             if(prices[i] > prices[i-1]){
