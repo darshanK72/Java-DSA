e1034fd5408b23cf36d06bf212516842e0a4a788
Darshan Khairnar <darshankhairnar72@gmail.com>
2024-08-26 12:48:59 +0530

    few more diagonal traversal and Saddle point question

--- 08. Matrix/j21ZigZagDiagonalTraversal.java
+++ 08. Matrix/j21ZigZagDiagonalTraversal.java
@@ -12,11 +12,16 @@ public class j21ZigZagDiagonalTraversal {
                 matrix[i][j] = in.nextInt();
             }
         }
-        System.out.println(Arrays.toString(zigZagTraversal(matrix)));
+        if(row == col){
+            System.out.println(Arrays.toString(zigZagTraversalForSquareMatrix(matrix)));
+        }else{
+            System.out.println(Arrays.toString(zigZagTraversalForRactangleMatrix(matrix)));
+        }
         in.close();
     }
 
-    public static int[] zigZagTraversal(int[][] mat){
+    // O(N ^ 2)
+    public static int[] zigZagTraversalForSquareMatrix(int[][] mat){
         int n = mat.length; 
 
         int[] out = new int[n * n];
@@ -37,6 +42,34 @@ public class j21ZigZagDiagonalTraversal {
             }
         }
         return out;   
+    }
+
+    // O(M * N)
+    public static int[] zigZagTraversalForRactangleMatrix(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[] out = new int[m * n];
+        int i = 0;
+        for(int d = 0; d < m + n - 1; d++){
+            if(d % 2 == 1){
+                int row = d < n ? 0 : d - n + 1;
+                int col = d < n ? d : n - 1;
+                while(row < m & col >= 0){
+                    out[i++] = mat[row][col];
+                    row++;
+                    col--;
+                }
+            }else{
+                int row = d < m ? d : m - 1;
+                int col = d < m ? 0 : d - m + 1;
+                while(row >= 0 & col < n){
+                    out[i++] = mat[row][col];
+                    row--;
+                    col++;
+                }
+            }
+        }
+        return out;
     }
 
 }
