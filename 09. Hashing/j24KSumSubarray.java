703e2d775e4b80471328e1a90afed0a7bcc82c8f
Darshan Khairnar <darshankhairnar72@gmail.com>
2024-09-03 19:43:25 +0530

    soem changes in k sum subarray

--- 09. Hashing/j24KSumSubarray.java
+++ 09. Hashing/j24KSumSubarray.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class j24KSumSubarray {
 
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int[] arr = new int[n];
@@ -13,18 +13,18 @@ public class j24KSumSubarray {
             arr[i] = in.nextInt();
         }
         int k = in.nextInt();
-        System.out.println(subarraySumK(arr, n,k));
-        System.out.println(subarraySumKHashMap(arr, n,k));
+        System.out.println(subarraySumK(arr, n, k));
+        System.out.println(subarraySumKHashMap(arr, n, k));
         in.close();
     }
 
     public static ArrayList<Integer> subarraySumK(int[] arr, int n, int k) {
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             int s = 0;
-            for(int j = i; j < n; j++){
+            for (int j = i; j < n; j++) {
                 s += arr[j];
-                if(s == k){
-                    return new ArrayList<Integer>(Arrays.asList(i+1,j+1));
+                if (s == k) {
+                    return new ArrayList<Integer>(Arrays.asList(i + 1, j + 1));
                 }
             }
         }
@@ -32,16 +32,16 @@ public class j24KSumSubarray {
     }
 
     public static ArrayList<Integer> subarraySumKHashMap(int[] arr, int n, int k) {
-        HashMap<Integer,Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         int s = 0;
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++) {
             s += arr[i];
-            if(s == k){
-                return new ArrayList<Integer>(Arrays.asList(0,i));
-            }else if(map.containsKey(s - k)){
-                return new ArrayList<Integer>(Arrays.asList(map.get(s-k)+1,i+1));
-            }else{
-                map.put(s,i);
+            if (s == k) {
+                return new ArrayList<Integer>(Arrays.asList(1, i + 1));
+            } else if (map.containsKey(s - k)) {
+                return new ArrayList<Integer>(Arrays.asList(map.get(s - k) + 1, i + 1));
+            } else {
+                map.put(s, i + 1);
             }
         }
         return new ArrayList<Integer>(Arrays.asList(-1));
