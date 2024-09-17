a50bb6bc240be54ae7a2f7ec8d3e984f107c7bf7
Darshan Khairnar <darshankhairnar72@gmail.com>
2024-09-17 13:37:35 +0530

    completed ramaining hashing string questions

--- 10. Strings/03. String Matching/j12MostSpecificPattern.java
+++ 10. Strings/03. String Matching/j12MostSpecificPattern.java
@@ -1,3 +1,82 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class j12MostSpecificPattern {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        ArrayList<String> dict = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            dict.add(in.next());
+        }
+        String pattern = in.next();
+        System.out.println(findMatchedWords(n, dict, pattern));
+        System.out.println(findMatchedWordsEfficient(n, dict, pattern));
+        in.close();
+    }
+
+    public static ArrayList<String> findMatchedWords(int n, ArrayList<String> dict,
+            String pattern) {
+        ArrayList<String> out = new ArrayList<>();
+        for (String s : dict) {
+            if (isIsomorphic(s, pattern))
+                out.add(s);
+        }
+        return out;
+    }
+
+    public static boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+        HashMap<Character, Character> cmap = new HashMap<>();
+        HashMap<Character, Boolean> bmap = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+
+            if (cmap.containsKey(ch1)) {
+                if (cmap.get(ch1) != ch2)
+                    return false;
+            } else {
+                if (bmap.containsKey(ch2))
+                    return false;
+                else {
+                    cmap.put(ch1, ch2);
+                    bmap.put(ch2, true);
+                }
+            }
+        }
+        return true;
+    }
+
+    public static ArrayList<String> findMatchedWordsEfficient(int n, ArrayList<String> dict, String pattern) {
+        ArrayList<String> out = new ArrayList<>();
+        int[] lastIndex = new int[256];
+        String encodedPattern = encodeString(pattern, lastIndex);
+        System.out.println(encodedPattern);
+
+        for (String word : dict) {
+            if (word.length() == pattern.length() && encodeString(word, lastIndex).equals(encodedPattern)) {
+                out.add(word);
+            }
+        }
+        return out;
+    }
+
+    private static String encodeString(String str, int[] lastIndex) {
+        Arrays.fill(lastIndex, -1);
+        StringBuilder encoded = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (lastIndex[c] == -1) {
+                lastIndex[c] = i;
+            }
+            encoded.append(lastIndex[c]);
+        }
+        return encoded.toString();
+    }
 
 }
--- 10. Strings/03. String Matching/j15WordSubsets.java
+++ 10. Strings/03. String Matching/j15WordSubsets.java
@@ -1,3 +1,96 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+
 public class j15WordSubsets {
-    
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n1 = in.nextInt();
+        String[] words1 = new String[n1];
+        for (int i = 0; i < n1; i++) {
+            words1[i] = in.next();
+        }
+        int n2 = in.nextInt();
+        String[] words2 = new String[n2];
+        for (int i = 0; i < n2; i++) {
+            words2[i] = in.next();
+        }
+        System.out.println(wordSubsets(words1, words2));
+        System.out.println(wordSubsetsEfficient(words1, words2));
+        in.close();
+    }
+
+    public static List<String> wordSubsets(String[] words1, String[] words2) {
+        ArrayList<HashMap<Character, Integer>> mapList = new ArrayList<>();
+        for (String word : words2) {
+            HashMap<Character, Integer> map = new HashMap<>();
+            for (char c : word.toCharArray()) {
+                map.put(c, map.getOrDefault(c, 0) + 1);
+            }
+            mapList.add(map);
+        }
+
+        ArrayList<String> out = new ArrayList<>();
+        for (String word : words1) {
+            HashMap<Character, Integer> tmap = new HashMap<>();
+            for (char c : word.toCharArray()) {
+                tmap.put(c, tmap.getOrDefault(c, 0) + 1);
+            }
+            boolean isUni = true;
+            for (HashMap<Character, Integer> map : mapList) {
+                if (!isUniversal(tmap, map)) {
+                    isUni = false;
+                    break;
+                }
+            }
+            if (isUni)
+                out.add(word);
+        }
+        return out;
+    }
+
+    public static boolean isUniversal(HashMap<Character, Integer> tmap, HashMap<Character, Integer> map) {
+        for (char key : map.keySet()) {
+            if (!tmap.containsKey(key) || tmap.get(key) < map.get(key)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static List<String> wordSubsetsEfficient(String[] words1, String[] words2) {
+        int[] maxFreq = new int[26];
+        for (String s : words2) {
+            int[] freq = new int[26];
+            for (char c : s.toCharArray()) {
+                freq[c - 'a']++;
+            }
+
+            for (int i = 0; i < 26; i++) {
+                maxFreq[i] = Math.max(maxFreq[i], freq[i]);
+            }
+        }
+
+        ArrayList<String> out = new ArrayList<>();
+        for (String word : words1) {
+            int[] freq = new int[26];
+            for (char c : word.toCharArray()) {
+                freq[c - 'a']++;
+            }
+
+            boolean isUniversal = true;
+            for (int i = 0; i < 26; i++) {
+                if (freq[i] < maxFreq[i]) {
+                    isUniversal = false;
+                    break;
+                }
+            }
+
+            if (isUniversal) {
+                out.add(word);
+            }
+        }
+        return out;
+    }
 }
