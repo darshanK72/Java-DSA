dfdbb1299a155f4a0016995175309e3c548afe36
Darshan Khairnar <darshankhairnar72@gmail.com>
2024-09-17 16:46:04 +0530

    rearranged recursion and bit mainipulation questions

--- 05. Bit Manipulation/04. Other Bit Problems/j03JosephusProblem.java
+++ 05. Bit Manipulation/04. Other Bit Problems/j03JosephusProblem.java
@@ -1,19 +1,26 @@
 import java.util.Scanner;
+
 public class j03JosephusProblem {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         System.out.println(josephusProb(n));
         in.close();
     }
 
-    public static int josephusProb(int n){
+    public static int joesephKill(int n, int k) {
+        if (n == 0)
+            return 0;
+        return (joesephKill(n - 1, k) + k) % n;
+    }
+
+    public static int josephusProb(int n) {
         int p = 1;
-        while(p * 2 < n){
+        while (p * 2 < n) {
             p *= 2;
         }
         System.out.println(p);
         int l = n - p;
-        return 2*l+1;
+        return 2 * l + 1;
     }
 }
