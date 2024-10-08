259203195f15e72379ab27bbfc1c0c0e6351c5b8
Darshan Khairnar <darshankhairnar72@gmail.com>
2024-10-08 12:06:48 +0530

    started with binary search

---
+++ 12. Searching/01. Binary Search Basics/j01BinarySearch.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class j01BinarySearch {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        int k = in.nextInt();
+        System.out.println(binarySearch(arr, k));
+        System.out.println(binarySearchAnyOrder(arr, k));
+        in.close();
+    }
+
+    public static int binarySearch(int[] nums, int target) {
+        int s = 0;
+        int e = nums.length - 1;
+        while (s <= e) {
+            int mid = (s + e) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public static int binarySearchAnyOrder(int[] nums, int target) {
+        int s = 0;
+        int e = nums.length - 1;
+        boolean isAscending = nums[s] < nums[e] ? true : false;
+        while (s <= e) {
+            int mid = (s + e) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (isAscending) {
+                if (nums[mid] > target) {
+                    e = mid - 1;
+                } else {
+                    s = mid + 1;
+                }
+            } else {
+                if (nums[mid] > target) {
+                    s = mid + 1;
+                } else {
+                    e = mid - 1;
+                }
+            }
+
+        }
+        return -1;
+    }
+}
