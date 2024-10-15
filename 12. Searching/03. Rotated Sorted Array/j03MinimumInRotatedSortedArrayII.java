0dc4280efd130a424c5b23a9fb32c5663ba6b01c
Darshan Khairnar <darshankhairnar72@gmail.com>
2024-10-15 23:03:26 +0530

    solved one question on binary search rotated and sorted

--- 12. Searching/03. Rotated Sorted Array/j03MinimumInRotatedSortedArrayII.java
+++ 12. Searching/03. Rotated Sorted Array/j03MinimumInRotatedSortedArrayII.java
@@ -1,32 +1,15 @@
 import java.util.Scanner;
 
-public class j02MinimumInRotatedSortedArrayI {
-    public static void main(String args[]) {
+public class j03MinimumInRotatedSortedArrayII {
+     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = in.nextInt();
         }
-        System.out.println(findMin1(arr));
         System.out.println(findMin2(arr));
         in.close();
-    }
-
-    public static int findMin1(int[] nums) {
-        int s = 0;
-        int e = nums.length - 1;
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
-            if (nums[mid] >= nums[0]) {
-                s = mid + 1;
-            } else {
-                e = mid - 1;
-            }
-        }
-        if (s == nums.length)
-            return nums[0];
-        return nums[s];
     }
 
     public static int findMin2(int[] nums) {
---
+++ 12. Searching/03. Rotated Sorted Array/j04SearchInRotatedSortedArrayI.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class j04SearchInRotatedSortedArrayI {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        int target = in.nextInt();
+        System.out.println(search(arr, target));
+        in.close();
+    }
+
+    public static int search(int[] nums, int target) {
+        int s = 0;
+        int e = nums.length - 1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (nums[mid] >= nums[0]) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
+        }
+        if (s == nums.length) {
+            return binarySearch(nums, 0, nums.length - 1, target);
+        } else if (target >= nums[0]) {
+            return binarySearch(nums, 0, e, target);
+        } else {
+            return binarySearch(nums, s, nums.length - 1, target);
+        }
+    }
+
+    public static int binarySearch(int[] nums, int s, int e, int target) {
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] > target) {
+                e = mid - 1;
+            } else {
+                s = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
